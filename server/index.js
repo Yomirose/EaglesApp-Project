090ee7c24a750c8d962ed4ecfb600194dfd1cff6
Yomirose <166441929+Yomirose@users.noreply.github.com>
2024-10-25 11:47:01 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(cookiesParser());
 
 app.use(cors({
-    origin: https://eaglesapp-frontend.onrender.com, 
+    origin: process.env.FRONTEND_URL, 
     credentials: true
 }));
 
@@ -41,7 +41,7 @@ app.use(cors({
 // Socket.io configuration
 const io = new Server(server, {
     cors: {
-        origin: https://eaglesapp-frontend.onrender.com,
+        origin: process.env.FRONTEND_URL,
         methods: ["GET", "POST"],
         credentials: true
     },
