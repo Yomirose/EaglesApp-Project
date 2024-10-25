dcc4e3e7e6eec49272de0e2d1d2822bb8c392f79
Yomirose <166441929+Yomirose@users.noreply.github.com>
2024-10-25 10:31:05 +0100

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -18,7 +18,7 @@ const Home = () => {
 
   const fetchUserDetails = useCallback(async () => {
     try {
-      const URL = `${process.env.REACT_APP_BACKEND_URL}/api/user-details`;
+      const URL = `${https://eaglesapp-backend.onrender.com}/api/user-details`;
       const response = await axios({
         url: URL,
         withCredentials: true
@@ -40,7 +40,7 @@ const Home = () => {
   }, [fetchUserDetails]);
 
   useEffect(() =>{
-     const socketConnection = io(process.env.REACT_APP_BACKEND_URL,{
+     const socketConnection = io(https://eaglesapp-backend.onrender.com,{
       auth: {
         token: localStorage.getItem("token")
       }
