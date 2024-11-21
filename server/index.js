a5a4942a04541cecb6bbccf2657b5403937000d0
Yomirose <roselinefayomi10@yahoo.comcleargit config --global user.email roselinefayomi10@yahoo.com>
2024-11-21 19:17:11 +0100

    update deploy

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const UserModel = require("./models/UserModel");
 const ConversationModel = require("./models/ConversationModel");
 const MessageModel = require("./models/MessageModel");
 const getConversation = require("./helpers/getConversation");
-const path = require("path");
+// const path = require("path");
 
 dotenv.config();
 const app = express();
@@ -37,20 +37,20 @@ app.use(cors({
 }));
 
 // ...........deployment code ...........//
-const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === "production") {
+// const __dirname1 = path.resolve();
+// if (process.env.NODE_ENV === "production") {
 
-    app.use(express.static(path.join(__dirname1,"../client/build")));
+//     app.use(express.static(path.join(__dirname1,"../client/build")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname1, "../client/build", "index.html"));
-    })
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname1, "../client/build", "index.html"));
+//     })
 
-} else {
-    app.get("/", (req, res) => {
-        res.send("API is Running Successfully")
-    });
-}
+// } else {
+//     app.get("/", (req, res) => {
+//         res.send("API is Running Successfully")
+//     });
+// }
 // ...........deployment code ...........//
 
 
